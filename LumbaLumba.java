25e2a3d0dd98d6a1ed0f6568813e3eb50a3fd69a
Pramudya31 <85573677+Pramudya31@users.noreply.github.com>
2021-06-10 01:16:32 +0700

    Add files via upload

---
+++ LumbaLumba.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author YOGA
+ */
+public class LumbaLumba extends Kucing {
+
+    String nama1 = "LumbaLumba";
+    String tempat1 = "Air";
+    String caragerak1 = "Berenang";
+    String jenis1 = "hewan";
+
+    public void showinfo() {
+        System.out.println("nama / nama1 : " + super.nama);
+        System.out.println("tempat / tempat1 : " + super.tempat);
+        System.out.println("caragerak / caragerak1 : " + caragerak1);
+        System.out.println("jenis / jenis1 : " + jenis);
+    }
+}
